3c91dc8b32fb9bd7b18d7602061131f74cf0d0f6
Max Sherlock <sherlockmax@users.noreply.github.com>
2026-03-20 16:08:10 +0800

    Update rand_team.js

--- js/rand_team.js
+++ js/rand_team.js
@@ -1,28 +1,31 @@
-var app = new Vue({
+new Vue({
   el: '#app',
-  data: {
-    loading: false,
-    members: [],
-    teams: [],
-    teamHistory: [],
-    tagType: ['', 'success', 'info', 'warning', 'danger'],
-    times: ['18:00 ~ 18:30', '18:30 ~ 19:00', '19:00 ~ 19:30'],
-    dialogFormVisible: false,
-    form: {
-      title: '羽球團',
-      date: '2021.02.26',
-      memberTxt: 'andy\njoey\nqueen\nking'
-    },
-    formLabelWidth: '120px'
+  data: function () {
+    return {
+      loading: false,
+      members: [],
+      teams: [],
+      teamHistory: [],
+      tagType: ['', 'success', 'info', 'warning', 'danger'],
+      times: ['18:00 ~ 18:30', '18:30 ~ 19:00', '19:00 ~ 19:30'],
+      dialogFormVisible: false,
+      form: {
+        title: '羽球團',
+        date: '2021.02.26',
+        memberTxt: 'andy\njoey\nqueen\nking'
+      },
+      formLabelWidth: '120px'
+    }
   },
-  mounted() {
-    const now = new Date()
-    const y = now.getFullYear()
-    const m = (now.getMonth() + 1 + '').padStart(2, '0')
-    const d = (now.getDate() + '').padStart(2, '0')
+  mounted: function () {
+    var now = new Date()
+    var y = now.getFullYear()
+    var m = String(now.getMonth() + 1).padStart(2, '0')
+    var d = String(now.getDate()).padStart(2, '0')
+
     this.form.date = y + '.' + m + '.' + d
 
-    const originSetting = Cookies.get('MaxBGGroupSetting')
+    var originSetting = Cookies.get('MaxBGGroupSetting')
 
     if (originSetting) {
       this.form = JSON.parse(originSetting)
@@ -31,17 +34,17 @@ var app = new Vue({
     this.changeSettings()
   },
   methods: {
-    goHome: function() {
+    goHome: function () {
       window.location.href = 'index.html'
     },
     makeTeams: function (fixedTeam) {
-      let arr = this.shuffleArray(this.members)
-      let team = []
-      let teams = []
+      var arr = this.shuffleArray(this.members)
+      var team = []
+      var teams = []
 
       if (fixedTeam.length > 0) {
-        fixedTeam.forEach(p => {
-          const index = arr.indexOf(p)
+        fixedTeam.forEach(function (p) {
+          var index = arr.indexOf(p)
           if (index > -1) {
             arr.splice(index, 1)
           }
@@ -50,25 +53,29 @@ var app = new Vue({
         })
       }
 
-      arr.forEach(p => {
-        team.push(p)
-        if (team.length >= 2) {
-          team.sort()
-          if (!this.isDuplicate(team)) {
-            this.teamHistory.push(team)
-            teams.push(team)
+      arr.forEach(
+        function (p) {
+          team.push(p)
+          if (team.length >= 2) {
+            team.sort()
+            if (!this.isDuplicate(team)) {
+              this.teamHistory.push(team)
+              teams.push(team)
+            }
+            team = []
           }
-          team = []
-        }
-      })
+        }.bind(this)
+      )
 
       teams = this.shuffleArray(teams)
-      let teamLatestIndex = teams.length-1
-      teams.forEach((t, i) => {
-        if (i == teamLatestIndex) {
+      var teamLatestIndex = teams.length - 1
+
+      teams.forEach(function (t, i) {
+        if (i === teamLatestIndex) {
           return
         }
-        if (t[0] == '<->' || t[1] == '<->') {
+
+        if (t[0] === '<->' || t[1] === '<->') {
           teams[i] = teams[teamLatestIndex]
           teams[teamLatestIndex] = t
         }
@@ -77,21 +84,24 @@ var app = new Vue({
       return teams
     },
     isDuplicate: function (team) {
-      let result = false
-      this.teamHistory.forEach(oTeam => {
-        if (JSON.stringify(oTeam) == JSON.stringify(team)) {
+      var result = false
+
+      this.teamHistory.forEach(function (oTeam) {
+        if (JSON.stringify(oTeam) === JSON.stringify(team)) {
           result = true
-          return
         }
       })
 
       return result
     },
     shuffleArray: function (originArr) {
-      let arr = this.deepClone(originArr)
-      for (let i = originArr.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1))
-        ;[arr[i], arr[j]] = [arr[j], arr[i]]
+      var arr = this.deepClone(originArr)
+
+      for (var i = originArr.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1))
+        var temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
       }
 
       return arr
@@ -100,29 +110,37 @@ var app = new Vue({
       return JSON.parse(JSON.stringify(arr))
     },
     changeSettings: function () {
-      this.loading = true
-      this.members = this.form.memberTxt.split('\n')
+      var _this = this
 
-      if (this.members.length % 2 == 1) {
+      this.loading = true
+      this.members = this.form.memberTxt.split('\n').filter(function (member) {
+        return member.trim() !== ''
+      })
+
+      if (this.members.length % 2 === 1) {
         this.members.push('<->')
       }
 
       this.members.sort()
       this.teams = []
       this.teamHistory = []
-      setTimeout(() => {
-        let wholeTeams = []
-        let maxCount = 50
+
+      setTimeout(function () {
+        var wholeTeams = []
+        var maxCount = 50
+
         while (wholeTeams.length < 3) {
           maxCount--
-          teams = this.makeTeams([])
-          if (teams.length == this.members.length / 2) {
+
+          var teams = _this.makeTeams([])
+
+          if (teams.length === _this.members.length / 2) {
             wholeTeams.push(teams)
           } else {
-            teams.forEach(team => {
-              const index = this.teamHistory.indexOf(team)
+            teams.forEach(function (team) {
+              var index = _this.teamHistory.indexOf(team)
               if (index > -1) {
-                this.teamHistory.splice(index, 1)
+                _this.teamHistory.splice(index, 1)
               }
             })
           }
@@ -132,12 +150,12 @@ var app = new Vue({
           }
         }
 
-        Cookies.set('MaxBGGroupSetting', JSON.stringify(this.form))
+        Cookies.set('MaxBGGroupSetting', JSON.stringify(_this.form))
 
-        setTimeout(() => {
-          this.dialogFormVisible = false
-          this.teams = wholeTeams
-          this.loading = false
+        setTimeout(function () {
+          _this.dialogFormVisible = false
+          _this.teams = wholeTeams
+          _this.loading = false
         }, 500)
       }, 500)
     }
